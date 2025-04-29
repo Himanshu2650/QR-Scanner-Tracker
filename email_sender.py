8c2d40a4eba6c6ceb6818a8833016c420f95f146
Himanshu kumar <156792439+Himanshu2650@users.noreply.github.com>
2025-04-29 22:25:24 +0530

    Add files via upload

--- email_sender.py
+++ email_sender.py
@@ -4,8 +4,8 @@
 # def send_email_with_map(attachment_path):
 #     msg = EmailMessage()
 #     msg['Subject'] = 'Walk Tracker Report'
-#     msg['From'] = ''
-#     msg['To'] = [" "]
+#     msg['From'] = 'himanshukumaranand3670@gmail.com'
+#     msg['To'] = ["thakuranand2650@gmail.com"]
 
 #     msg.set_content('Please find attached the walk tracker report.')
 
@@ -16,7 +16,7 @@
 #     # Using TLS (recommended)
 #     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
 #         smtp.starttls()
-#         smtp.login('Email', 'password')
+#         smtp.login('himanshukumaranand3670@gmail.com', 'ocki cuvx xlxp phva')
 #         smtp.send_message(msg)
 
 
@@ -26,8 +26,8 @@ from email.message import EmailMessage
 def send_email_with_map(attachment_path):
     msg = EmailMessage()
     msg['Subject'] = 'Walk Tracker Report'
-    msg['From'] = ' '  # Use your email here
-    msg['To'] = [" "]  # Use the recipient's email
+    msg['From'] = 'himanshukumaranand3670@gmail.com'  # Use your email here
+    msg['To'] = ["thakuranand2650@gmail.com"]  # Use the recipient's email
 
     msg.set_content('Please find attached the walk tracker report.')
 
@@ -38,5 +38,5 @@ def send_email_with_map(attachment_path):
     # Using TLS (recommended)
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.starttls()
-        smtp.login(' ', ' ')  # Use your actual credentials
+        smtp.login('himanshukumaranand3670@gmail.com', 'ocki cuvx xlxp phva')  # Use your actual credentials
         smtp.send_message(msg)
